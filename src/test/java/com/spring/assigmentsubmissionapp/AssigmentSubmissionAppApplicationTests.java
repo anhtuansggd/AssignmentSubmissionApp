6f690997e77bc0de9a6a49d517e33fba7e71b846
anhtuansggd <anhtuan.sggd@gmail.com>
2024-02-19 08:58:56 +0700

    build: setting up spring boot dependencies and application properties

---
+++ src/test/java/com/spring/assigmentsubmissionapp/AssigmentSubmissionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.assigmentsubmissionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentSubmissionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
