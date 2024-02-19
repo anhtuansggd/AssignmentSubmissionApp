3e3c325696d03940adaa3dbaccbb637a2762eadf
anhtuansggd <anhtuan.sggd@gmail.com>
2024-02-19 10:57:55 +0700

    feat(domain): add User and Assignment entities and update application properties
    Remove PostgresqlDialect from properties

---
+++ src/main/java/com/spring/assigmentsubmissionapp/domain/User.java
@@ -0,0 +1,53 @@
+package com.spring.assigmentsubmissionapp.domain;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users") //User is reserved word, so create a new table manually
+public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate cohortStartDate;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCohortStartDate() {
+        return cohortStartDate;
+    }
+
+    public void setCohortStartDate(LocalDate cohortStartDate) {
+        this.cohortStartDate = cohortStartDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
