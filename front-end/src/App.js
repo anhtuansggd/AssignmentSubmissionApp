ac992f644cef1414605455cbfbaf8b0bd43ae112
anhtuansggd <anhtuan.sggd@gmail.com>
2024-02-23 17:52:12 +0700

    feat(front-end): store JWT in LocalStorage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import {useLocalState} from "./util/useLocalStorage";
 
 function App() {
+    const [jwt, setJwt] = useLocalState("","jwt");
+    useEffect(() => {
+        if(!jwt){
+            const reqBody = {
+                username: "test",
+                password: "asdfasdf",
+            };
 
-  const reqBody = {
-    username: "test",
-    password: "asdfasdf",
-  };
-
-  fetch("api/auth/login", {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    method: "post",
-    body: JSON.stringify(reqBody),
-  })
-      .then((response) => Promise.all([response.json(), response.headers]))
-      .then(([body, headers]) => {
-          const authValue = headers.get("authorization");
-          console.log(authValue);
-      }
-    );
+            fetch("api/auth/login", {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "post",
+                body: JSON.stringify(reqBody),
+            })
+                .then((response) => Promise.all([response.json(), response.headers]))
+                .then(([body, headers]) => {
+                    setJwt(headers.get("authorization"));
+                });
+        }
+    }, []);
 
   return(
       <div className="App">
-        <h1>Hello World</h1>
+        <h1>JWT value is {jwt}</h1>
       </div>
   );
 }
