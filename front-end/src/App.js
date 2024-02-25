4bd1d554c4caacff19fa183126c230f3e1de2d32
anhtuansggd <anhtuan.sggd@gmail.com>
2024-02-26 00:00:08 +0700

    feat: connect login page with api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,26 +9,7 @@ import PrivateRoute from "./PrivateRoute";
 
 function App() {
     const [jwt, setJwt] = useLocalState("","jwt");
-    // useEffect(() => {
-    //     if(!jwt){
-    //         const reqBody = {
-    //             username: "test",
-    //             password: "asdfasdf",
-    //         };
-    //
-    //         fetch("api/auth/login", {
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             },
-    //             method: "post",
-    //             body: JSON.stringify(reqBody),
-    //         })
-    //             .then((response) => Promise.all([response.json(), response.headers]))
-    //             .then(([body, headers]) => {
-    //                 setJwt(headers.get("authorization"));
-    //             });
-    //     }
-    // }, []);
+
 
   return(
       <Routes>
