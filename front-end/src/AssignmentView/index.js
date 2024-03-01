64084965dee5e4aa3db0622a9e10159b4bc5f24c
anhtuansggd <anhtuan.sggd@gmail.com>
2024-03-01 23:03:44 +0700

    feat: prettier AssignmentView

--- front-end/src/AssignmentView/index.js
+++ front-end/src/AssignmentView/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {useLocalState} from "../util/useLocalStorage";
 import {json} from "react-router-dom";
 import ajax from "../Services/fetchService";
+import {Badge, Button, ButtonGroup, Col, Container, Dropdown, DropdownButton, Form, Row} from "react-bootstrap";
 
 const AssignmentView = () => {
     const assignmentId = window.location.href.split("/assignments/")[1];
@@ -13,14 +14,14 @@ const AssignmentView = () => {
 
 
     function updateAssignment(prop, value) {
-       const newAssignment = {...assignment};
+        const newAssignment = {...assignment};
         newAssignment[prop] = value;
         setAssignment(newAssignment);
     }
 
     function save() {
         ajax(`/api/assignments/${assignmentId}`, "PUT", jwt, assignment).then(
-            (assignmentData) =>{
+            (assignmentData) => {
                 setAssignment(assignmentData);
             });
     }
@@ -28,31 +29,74 @@ const AssignmentView = () => {
     useEffect(() => {
         ajax(`/api/assignments/${assignmentId}`, "GET", jwt).then(
             (assignmentData) => {
-                if(assignmentData.branch === null)
+                if (assignmentData.branch === null)
                     assignmentData.branch = "";
-                if(assignmentData.githubUrl === null)
+                if (assignmentData.githubUrl === null)
                     assignmentData.githubUrl = "";
-            setAssignment(assignmentData);
-        });
+                setAssignment(assignmentData);
+            });
     }, []);
     return (
-        <div>
-            <h1>Assignment {assignmentId}</h1>
+        <Container className="mt-5">
+            <Row className="d-flex align-items-center">
+                <Col>
+                    <h1>Assignment {assignmentId}</h1>
+                </Col>
+                <Col>
+                    <Badge pill bg="info" style={{fontSize: "1em"}}>
+                        {assignment.status}
+                    </Badge>
+                </Col>
+            </Row>
+
+
             {assignment ? <>
-                <h2>Status: {assignment.status}</h2>
-                <h3>Github URL: <input type="url" id="githubUrl"
-                                       onChange={(event) =>
-                                           updateAssignment("githubUrl", event.target.value)}
-                                            value={assignment.githubUrl}
-                /></h3>
-                <h3>Branch: <input type="text" id="branch"
-                                   onChange={((event) =>
-                                       updateAssignment("branch", event.target.value))}
-                                        value={assignment.branch}
-                /></h3>
+                <Form.Group as={Row} className="my-4" controlId="formPlaintextEmail">
+                    <Form.Label column sm="3" md="2">
+                        Assignment number:
+                    </Form.Label>
+                    <Col sm="9" md="8" lg="6">
+                        <DropdownButton
+                            as={ButtonGroup}
+                            id="assignmentName"
+                            variant={"info"}
+                            title={"Assignment 1"}>
+                            {['1','2','3','4','5','6'].map(assignmentNum =>
+                                <Dropdown.Item eventKey={assignmentNum}>{assignmentNum}</Dropdown.Item>
+                            )}
+                        </DropdownButton>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="my-4" controlId="formPlaintextEmail">
+                    <Form.Label column sm="3" md="2">
+                        Github URL:
+                    </Form.Label>
+                    <Col sm="9" md="8" lg="6">
+                        <Form.Control id="githubUrl"
+                                      onChange={(event) =>
+                                          updateAssignment("githubUrl", event.target.value)}
+                                      value={assignment.githubUrl}
+                                      type="url" placeholder="https//github.com/username/repo-name"/>
+                    </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="3" md="2" lg="6">
+                        Branch:
+                    </Form.Label>
+                    <Col sm="9" md="8">
+                        <Form.Control id="branch"
+                                      onChange={((event) =>
+                                          updateAssignment("branch", event.target.value))}
+                                      value={assignment.branch}
+                                      type="text" placeholder="branch_name"/>
+                    </Col>
+                </Form.Group>
+
             </> : <></>}
-            <button onClick={() => save()}>Submit Assignment</button>
-        </div>
+            <Button size="lg" onClick={() => save()}>Submit Assignment</Button>
+        </Container>
     );
 };
 
